f9ed351ec5f96393a5c59f3f77a7f355750269fe
Muasa-harman <harman.muasa@gmail.com>
2024-10-31 19:40:16 +0300

    server setup

--- backend-implementation/src/server.ts
+++ backend-implementation/src/server.ts
@@ -3,14 +3,14 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import b2bRoutes from './routes/b2bRoutes';
-// import mpesaRoutes from './routes/mpesaRoutes';
-// import b2cRoutes from './routes/b2cRoutes';
-// import b2bExpressCheckoutRoutes from './routes/b2bExpressCheckoutRoutes';
-// import callbackRoutes from './routes/callbackRoutes';
-// import stkPushQueryRoutes from './routes/stkPushQueryRoutes';
-// import c2bTransactionRoutes from './routes/c2bTransactionRoutes';
-// import stkPushRoutes from './routes/stkPushRoutes';
-// import taxRemittanceRoutes from './routes/taxRemittanceRoutes';
+import mpesaRoutes from './routes/mpesaRoutes';
+import b2cRoutes from './routes/b2cRoutes';
+import b2bExpressCheckoutRoutes from './routes/b2bExpressCheckoutRoutes';
+import callbackRoutes from './routes/callbackRoutes';
+import stkPushQueryRoutes from './routes/stkPushQueryRoutes';
+import c2bTransactionRoutes from './routes/c2bTransactionRoutes';
+import stkPushRoutes from './routes/stkPushRoutes';
+import taxRemittanceRoutes from './routes/taxRemittanceRoutes';
 
 dotenv.config();
 
@@ -35,13 +35,13 @@ app.use(express.json());
 // //  routes 
 // app.use('/api/', mpesaRoutes)
 app.use('/api/', b2bRoutes);
-// app.use('/api/',b2cRoutes);
-// app.use('/api/',b2bExpressCheckoutRoutes)
-// app.use('/api/',callbackRoutes)
-// app.use('/api', stkPushQueryRoutes);
-// app.use('/api', c2bTransactionRoutes);
-// app.use('/api', stkPushRoutes);
-// app.use('/api', taxRemittanceRoutes);
+app.use('/api/',b2cRoutes);
+app.use('/api/',b2bExpressCheckoutRoutes)
+app.use('/api/',callbackRoutes)
+app.use('/api', stkPushQueryRoutes);
+app.use('/api', c2bTransactionRoutes);
+app.use('/api', stkPushRoutes);
+app.use('/api', taxRemittanceRoutes);
 
 // Start the server
 app.listen(port, () => {
